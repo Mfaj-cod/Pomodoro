b580d86f3807fc4392edcb1116688d295c4a1308
M. fajal <fajalmoaj@gmail.com>
2025-08-24 10:38:07 +0530

    edited manifest

--- static/service-worker.js
+++ static/service-worker.js
@@ -14,3 +14,16 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installing.");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activating.");
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
